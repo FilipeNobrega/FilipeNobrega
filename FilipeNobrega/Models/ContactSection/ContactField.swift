bebe00365acd63595a39f2d085fce4e28575c407
Filipe Nobrega <filipegnobrega@gmail.com>
2018-06-23 15:10:50 -0300

    Contact Section

---
+++ FilipeNobrega/Models/ContactSection/ContactField.swift
@@ -0,0 +1,14 @@
+//
+//  ContactField.swift
+//  FilipeNobrega
+//
+//  Created by Filipe Nobrega on 23/06/18.
+//  Copyright © 2018 Filipe. All rights reserved.
+//
+
+import Foundation
+
+struct ContactField: Decodable {
+  var icon: String?
+  var text: String?
+}
---
+++ FilipeNobrega/Models/ContactSection/ContactInfo.swift
@@ -0,0 +1,19 @@
+//
+//  ContactInfo.swift
+//  FilipeNobrega
+//
+//  Created by Filipe Nobrega on 23/06/18.
+//  Copyright © 2018 Filipe. All rights reserved.
+//
+
+import Foundation
+
+struct ContactInfo: Decodable {
+  let avatar: String?
+  let fields: [ContactInfo]?
+
+  var avatarUrl: URL? {
+    guard let avatar = avatar else { return nil }
+    return URL(string: avatar)
+  }
+}
