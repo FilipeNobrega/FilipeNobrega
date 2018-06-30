31961bde768d957760610f9b8c49cdc3e6c7aa61
Filipe Nobrega <filipegnobrega@gmail.com>
2018-06-30 18:16:22 -0300

    Adding sequentialLoadingView and base lottie, now the home has loading state

---
+++ FilipeNobrega/Utils/SequentialLoadingView.swift
@@ -0,0 +1,79 @@
+//
+//  SequenceLoadingView.swift
+//  FilipeNobrega
+//
+//  Created by Filipe Nobrega on 30/06/18.
+//  Copyright © 2018 Filipe. All rights reserved.
+//
+
+import Lottie
+import UIKit
+
+final class SequenceLoadingView: UIView {
+  private var animationView = LOTAnimationView(name: "animation")
+  private var isLoadingFinished = false
+  override var frame: CGRect {
+    didSet {
+      updateAnimationViewFrame()
+    }
+  }
+
+  init() {
+    super.init(frame: .zero)
+    setupAnimation()
+  }
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupAnimation()
+  }
+
+  @available(*, unavailable)
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  public func loadFinished() {
+    isLoadingFinished = true
+  }
+
+  private func updateAnimationViewFrame() {
+    let length = (bounds.width > bounds.height ? bounds.height : bounds.width) * 2 / 3
+    animationView.frame = CGRect(origin: .zero, size: CGSize(width: length, height: length))
+    animationView.center = bounds.center
+  }
+
+  private func setupAnimation() {
+    updateAnimationViewFrame()
+    addSubview(animationView)
+    animationView.loopAnimation = false
+    startSequence()
+  }
+
+  private func startSequence() {
+    animationView.play(toProgress: 0.2) { [weak self] _ in
+      self?.animationLoop()
+    }
+  }
+
+  private func animationLoop() {
+    animationView.play(fromProgress: 0.2, toProgress: 0.8) { [weak self] _ in
+      guard let strongSelf = self else { return }
+      if strongSelf.isLoadingFinished {
+        strongSelf.endSequence()
+      } else {
+        strongSelf.animationLoop()
+      }
+    }
+  }
+
+  private func endSequence() {
+    animationView.play(fromProgress: 0.8, toProgress: 1) { [weak self] _ in
+      UIView.animate(withDuration: 0.25, animations: { [weak self] in
+        self?.alpha = 0
+      }) { [weak self] _ in
+        self?.removeFromSuperview()
+      }
+    }
+  }
+}
